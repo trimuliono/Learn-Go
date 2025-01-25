65024c8e0c95527143d823759fd28179f0a2d30d
trimuliono <70k0b0d0@gmail.com>
2025-01-25 16:52:49 +0700

    update slice

--- CourseByUdemy/14_slice.go
+++ CourseByUdemy/14_slice.go
@@ -5,11 +5,18 @@ import "fmt"
 func main() {
 	names := [...]string{"Tri", "Muliono", "Ganteng", "Banget", "Gaada", "Obatnya"}
 
-	slice := names[2:6]
+	slice1 := names[2:6]
+	fmt.Println(slice1) // [Ganteng Banget Gaada Obatnya]
+	fmt.Println(slice1[0]) // Ganteng
+	fmt.Println(slice1[1]) // Banget
 
-	fmt.Println(slice[0]) // Ganteng
+	slice2 := names[:3] // Deklarasi slice tipe data string secara implisit karena array reference berupa string
+	fmt.Println(slice2) // [Tri Muliono Ganteng]
 
-	fmt.Println(slice[1]) // Banget
+	var slice3 []string = names[3:] // Deklarasi slice tipe data string secara eksplisit
+	fmt.Println(slice3) // [Banget Gaada Obatnya]
 
-	fmt.Println(slice) // [Ganteng Banget Gaada Obatnya]
+	var slice4 []string = names[:] // Deklarasi slice tipe data string secara eksplisit
+	fmt.Println(slice4) // [Tri Muliono Ganteng Banget Gaada Obatnya]
+
 }
