b3540197f8f8eabf33883628091eaf96a1a63c54
trimuliono <70k0b0d0@gmail.com>
2025-03-06 10:35:42 +0700

    add comment modifier

--- CourseByUdemy/helper/helper.go
+++ CourseByUdemy/helper/helper.go
@@ -2,8 +2,13 @@ package helper // nama package harus sama dengan nama folder
 
 import "fmt"
 
+// Access Modifier
+// Accesss modifier di golang diatur dengan cara penulisan nama variabel atau function
+// jika diawali huruf besar maka bisa diakses dari luar package
+// jika diawali huruf kecil maka hanya bisa diakses dari dalam package
+
 var version = "1.0.0" // variabel ini tidak bisa diakses dari luar package
-var Application = "golang"
+var Application = "golang" // variabel ini bisa diakses dari luar package
 
 func 
 SayGoodBye(name string) string{
